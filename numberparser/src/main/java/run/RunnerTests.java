baac8f449647c73e059a1262ef415f58901403ae
AIppolitov <franktanwork@googlemail.com>
2021-09-26 14:42:51 +0300

    Добавить тестовые примеры.

--- numberparser/src/main/java/run/RunnerTests.java
+++ numberparser/src/main/java/run/RunnerTests.java
@@ -5,18 +5,17 @@ import numberparser.factory.Parser;
 
 public class RunnerTests {
 	
-// покрыть код приличными тестами
 	public static void main(String[] args) {
 		String number1 = "8";
 		String number2 = "102000";
-		String phrase = "минус восемь";
-		Parser ipr = ParserSelector.getWanted(ParserSelector.DIGITS_TO_PHRASE);
-		
-		System.out.println(ipr.invert(number1));
-		System.out.println(ipr.invert(number2));
-		
-		ipr = ParserSelector.getWanted(ParserSelector.PHRASE_TO_DIGITS);
-		System.out.println(ipr.invert(phrase));
+		String phrase = "минус восемь октиллионов две тысячи одиннадцать";
+		Parser parser = ParserSelector.getWanted(ParserSelector.DIGITS_TO_PHRASE);
+
+		System.out.println(parser.invert(number1));
+		System.out.println(parser.invert(number2));
+
+		parser = ParserSelector.getWanted(ParserSelector.PHRASE_TO_DIGITS);
+		System.out.println(parser.invert(phrase));
 	}
 
 }
